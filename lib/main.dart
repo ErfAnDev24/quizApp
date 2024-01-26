104bd1ba25e2ac42b71e3ae130570af28fccf550
erfan <erfan01724@gmail.com>
2024-01-26 13:50:55 +0330

    quiz application

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text('ErfAn'),
+          ),
+        ),
+      ),
+    );
+  }
+}
