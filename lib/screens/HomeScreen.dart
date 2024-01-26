83fe6435cba611159750cad369a5c8b3aca11e06
erfan <erfan01724@gmail.com>
2024-01-26 14:14:12 +0330

    HomeScreen has been created!

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/screens/QuestionsScreen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('ErfAn'),
-        ),
-      ),
+      appBar: getAppBar(),
+      body: getMainContent(context),
     );
   }
+}
+
+PreferredSizeWidget getAppBar() {
+  return AppBar(
+    title: Text('Quiz game'),
+    backgroundColor: Colors.blue,
+    centerTitle: true,
+    foregroundColor: Colors.white,
+  );
+}
+
+Widget getMainContent(BuildContext context) {
+  return SafeArea(
+    child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      SizedBox(width: double.infinity),
+      SizedBox(
+        height: 30,
+      ),
+      Container(
+        width: 300,
+        height: 300,
+        child: Image(
+          image: AssetImage('images/welcome.png'),
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => QuestionScreen(),
+            ),
+          );
+        },
+        child: Text('Start!'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+    ]),
+  );
 }
